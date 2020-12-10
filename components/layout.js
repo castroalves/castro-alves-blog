c2c9c97058f9bae438490e95bbd6be1eabab46c3
Eduardo de Castro Alves <castro-alves@nextbike.com>
2020-12-10 23:46:23 +0100

    Add custom favicon generated by https://pixel-me.tokyo/en/

--- components/layout.js
+++ components/layout.js
@@ -10,10 +10,10 @@ export default function Layout({ children, home }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.png" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Here you'll find articles about PHP, JavaScript, NodeJS, Next.js, etc."
         />
         <meta
           property="og:image"
@@ -21,7 +21,7 @@ export default function Layout({ children, home }) {
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={siteTitle} />
+        <meta name="og:title" content="castroalves.dev" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className="flex flex-col p-8 justify-center">
