e23d52af94c54efa3bec49d06c8c6430882cca1f
Eduardo de Castro Alves <castro-alves@nextbike.com>
2020-12-14 01:43:37 +0100

    Added plugins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+const bundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+nextConfig = {
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
@@ -7,3 +13,19 @@ module.exports = {
     return config;
   },
 };
+
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        responsive: {
+          adapter: require("responsive-loader/sharp"),
+        },
+        optimizeImagesInDev: true,
+      },
+    ],
+    [bundleAnalyzer],
+  ],
+  nextConfig
+);
